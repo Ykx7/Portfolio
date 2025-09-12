b9f97b32d9025a1b92df33071487f4cc874284b4
Ykxx999 <143841023+Ykxx999@users.noreply.github.com>
2025-09-12 11:33:31 +0100

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './index.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
-import Framer from './Pages/learn-framer/framer'
 
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <>
           <Routes>
               <Route path='/' element={<Home />} />
-              <Route path='/test' element={<Framer />} />
           </Routes>
       </>
   )
