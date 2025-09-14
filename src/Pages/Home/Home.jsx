fcea4272be0df7bed1841bf18af2645f87399385
Ykxx999 <143841023+Ykxx999@users.noreply.github.com>
2025-09-14 11:01:01 +0100

    add cv

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -113,7 +113,13 @@ transition={{ duration: 0.5 }}
 whileHover={{ scale: 1.1 }}
 whileTap={{ scale: 0.9 }}
 transition={{ duration: 0.5 }}
->Download CV
+>
+  <a 
+  href="/cv.pdf" 
+  download="cv.pdf"
+>
+  Download CV
+</a>
 </MotionButton>
 </div>
 
